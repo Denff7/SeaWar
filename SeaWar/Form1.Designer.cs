45542ec4bf47fa49c2ccea547cb570d501f7ee9c
Админ <@DESKTOP-2QS55SB>
2024-05-12 15:26:29 +0300

    Добавьте файлы проекта.

---
+++ SeaWar/Form1.Designer.cs
@@ -0,0 +1,37 @@
+﻿namespace SeaWars
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+      
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+       
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
